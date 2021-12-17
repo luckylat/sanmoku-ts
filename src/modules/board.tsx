63486ef0d7a79a45fc3f02ff797be6183be43a10
luckYrat <lacklat.key@gmail.com>
2021-12-17 13:45:18 +0900

    関数型コンポーネントに変更

--- src/modules/board.tsx
+++ src/modules/board.tsx
@@ -3,36 +3,37 @@ import { Square } from "./square";
 
 type Hand = 'X'|'O'|null;
 
-interface Props{
+interface BoardProps{
   squares: Hand[],
   onClick: (arg0: number) => void
 }
 
-export default class Board extends React.Component<Props> {
+const Board = (props: BoardProps) => {
 
-  renderSquare(i: number) {
+
+  const renderSquare = (i: number) => {
     return (
-      <Square value={this.props.squares[i]} onClick={() => this.props.onClick(i)} key={i}/>
+      <Square value={props.squares[i]} onClick={() => props.onClick(i)} key={i}/>
     );
   }
 
-  render() {
-    return (
-      <div>
-        {
-          Array(3).fill(null).map((_,i) => {
-            return (
-              <div className="board-row" key={i}>
-                {
-                  Array(3).fill(null).map((_,j) => {
-                    return this.renderSquare(i*3+j)
-                  })
-                }
-              </div>
-            )
-          })
-        } 
-      </div>
-    );
-  }
+  return (
+    <div>
+      {
+        Array(3).fill(null).map((_,i) => {
+          return (
+            <div className="board-row" key={i}>
+              {
+                Array(3).fill(null).map((_,j) => {
+                  return renderSquare(i*3+j)
+                })
+              }
+            </div>
+          )
+        })
+      } 
+    </div>
+  );
 }
+
+export default Board;
--- src/modules/game.tsx
+++ src/modules/game.tsx
@@ -1,112 +1,75 @@
-import React from 'react';
+import React,{ useState } from 'react';
 import Board from "./board";
 
 import { calculateWinner } from "./calclatewinner";
 
-type Hand = 'X'|'O'|null;
-interface Props{
-  history?: {
-    squares: Hand[]
-    col: number
-    row: number
-  }[]
-  stepNumber?: number
-  xIsNext?: boolean
-}
-
-interface State{
-  history: {
-    squares: Hand[]
-    col: number
-    row: number
-  }[]
-  stepNumber: number
-  xIsNext: boolean
-}
+const Game = () => {
+  const [history, setHistory] = useState([{squares: Array(9).fill(null),col: 0, row: 0}]);
+  const [stepNumber, setStepNumber] = useState(0);
+  const [xIsNext, setXIsNext] = useState(true);
 
 
-export default class Game extends React.Component<Props,State>{
-  constructor(props: Props){
-    super(props);
-    this.state = {
-      history: [{
-        squares: Array(9).fill(null),
-        col: 0,
-        row: 0,
-      }],
-      stepNumber: 0,
-      xIsNext: true,
-    }
-  }
 
-  handleClick(i: number) {
-    const history = this.state.history.slice(0, this.state.stepNumber + 1);
-    const current = history[history.length-1];
+  const handleClick = (i: number) => {
+    const historyData = history.slice(0, stepNumber + 1);
+    const current = historyData[historyData.length-1];
     const squares = current.squares.slice();
     if(calculateWinner(squares) || squares[i]){
       return;
     }
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
-    this.setState({
-      history: history.concat([{
-        squares: squares,
-        col: (i-(i%3))/3+1,
-        row: i%3+1,
-      }]),
-      stepNumber: history.length,
-      xIsNext: !this.state.xIsNext,
-    });
-  }
-
-  jumpTo(step : number){
-
-    this.setState({
-      stepNumber: step,
-      xIsNext: (step % 2) === 0,
-    })
-
-    //太字にする
+    squares[i] = xIsNext ? 'X' : 'O';
+    setHistory(historyData.concat([{
+      squares: squares,
+      col: (i-(i%3))/3+1,
+      row: i%3+1
+    }]));
+    setStepNumber(historyData.length);
+    setXIsNext(!xIsNext);
 
   }
 
-  render() {
-    const history = this.state.history;
-    const current = history[this.state.stepNumber];
-    const winner = calculateWinner(current.squares);
+  const jumpTo = (step : number) => {
+    setStepNumber(step);
+    setXIsNext((step%2) === 0);
+  }
 
-    const moves = history.map((step,move) => {
-      const desc = move ? `Go to move #${move} (${step.col},${step.row})` : 'Go to game start';
-      return (
-        <li key={move}>
-          <button className={this.state.stepNumber === move?"button-active":""} onClick={() => {
-            this.jumpTo(move);
-            console.log(history);
-          }}>
-            {desc}
-          </button>
-        </li>
-      )
-    })
-    let status;
-    if(winner){
-      status = 'Winner is : ' + winner + '!!';
-    }else{
-      status = 'Next player is : ' + (this.state.xIsNext ? 'X' : 'O');
-    }
+  //太字にする
+  
+  const historyData = history;
+  const current = historyData[stepNumber];
+  const winner = calculateWinner(current.squares);
 
+  const moves = historyData.map((step,move) => {
+    const desc = move ? `Go to move #${move} (${step.col},${step.row})` : 'Go to game start';
     return (
-      <div className="game">
-        <div className="game-board">
-          <Board
-            squares={current.squares}
-            onClick={(i) => this.handleClick(i)}
+      <li key={move}>
+        <button className={stepNumber === move ? "button-active":""} onClick={() => {
+          jumpTo(move);
+          console.log(history);
+        }}>
+          {desc}
+        </button>
+      </li>
+    )
+  });
+  const status  = winner ? `Winner is ${winner} !!` : `Next player is ${xIsNext ? 'X' : '0'}`;
+  
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board
+          squares={current.squares}
+          onClick={(i) => handleClick(i)}
           />
-        </div>
-        <div className="game-info">
-          <div>{status}</div>
-          <ol>{moves}</ol>
-        </div>
       </div>
-    );
-  }
+      <div className="game-info">
+        <div>{status}</div>
+        <ol>{moves}</ol>
+      </div>
+    </div>
+  );
+  
+
 }
+
+export default Game
